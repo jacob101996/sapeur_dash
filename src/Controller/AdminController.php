ff1932818f267eb5654fc5935daec3062a9de74b
Onesime ZOUMA <onesime.zouma@iziconsultin.com>
2020-09-24 09:55:52 +0000

    Initialisation du projet

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/my/account/", name="admin_index")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
